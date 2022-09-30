12a8600933aa4011f08f8f6629b4d082b8bd2387
Numstr <Numstr@users.noreply.github.com>
2022-09-30 23:45:58 +0300

    refactor: change some default settings

--- App/DefaultData/Profile/user.js
+++ App/DefaultData/Profile/user.js
@@ -1,1 +1,34 @@
+////////// SETTINGS
+
+user_pref("browser.toolbars.bookmarks.visibility", newtab);
+
+user_pref("full-screen-api.transition-duration.enter", 0 0);
+user_pref("full-screen-api.transition-duration.leave", 0 0);
+
+user_pref("browser.compactmode.show", true);
+
+  ////////// GENERAL
+
+  user_pref("general.smoothScroll", false);
+  user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+
+  //////////
+
+  ////////// PRIVACY
+
+  user_pref("signon.generation.enabled", false);
+  user_pref("signon.management.page.breach-alerts.enabled", false);
+
+  user_pref("browser.urlbar.shortcuts.bookmarks", false);
+  user_pref("browser.urlbar.shortcuts.history", false);
+  user_pref("browser.urlbar.shortcuts.tabs", false);
+  user_pref("browser.urlbar.suggest.topsites", false);
+
+  // user_pref("media.autoplay.allow-extension-background-pages", false);
+  // user_pref("media.autoplay.block-event.enabled", true);
+
+  //////////
+
+//////////
+
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true); // [FF68+] allow userChrome/userContent
